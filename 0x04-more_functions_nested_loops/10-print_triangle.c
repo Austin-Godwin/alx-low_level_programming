a4eedb9bf0773505e3e185723aca2a1d09fa7198
Austin-Godwin <austingodwin18@gmail.com>
2022-09-18 18:02:59 -0700

    function that prints a triangle, followed by a new line

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -18,16 +18,20 @@ void print_triangle(int size)
 			{
 				_putchar(' ');
 			}
+
 			for (index = 0; index < hash; index++)
 			{
 				_putchar('#');
 			}
+
 			if (hash == size)
 			{
 				continue;
 			}
+
 			_putchar('\n');
 		}
 	}
+
 	_putchar('\n');
 }
