39159d5a7c5627f6854edb0f76f6b9ecd2968eac
Austin-Godwin <austingodwin18@gmail.com>
2022-09-21 18:24:26 -0700

    Write a function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Another repositiry for the continuation of C pointers, strings and arrays
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - Prints a character
+ * @c: a character in the _putchar function
+ * File: main.h
+ * Auth: Austin Godwin
+ * Desc: Header file containing prototypes for all functions
+ * used in the 0x06-pointers_arrays_strings.
+ *
+ * Return: No return values here, they are just a function prototype.
+ */
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
+#endif
